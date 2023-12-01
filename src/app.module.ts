080fc7eb80a9ae75aac0c57627bf433c0be0b7ce
Felipe Barrientos <barrientos.felipe@icloud.com>
2023-12-01 12:43:41 -0300

    commit detalles

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TodoModule } from './todo/todo.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://ACL:ACL123@aclcluster.1tsdljh.mongodb.net/todo?retryWrites=true&w=majority',
-    ),
-    TodoModule,
-  ],
+  imports: [MongooseModule.forRoot(process.env.DB_URI), TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
