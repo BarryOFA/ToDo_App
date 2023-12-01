7910920bea947f42cd9fa224de2121b7bfa414b4
Felipe Barrientos <barrientos.felipe@icloud.com>
2023-12-01 01:30:22 -0300

    Update nocturna 😪

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://ACL:ACL123@aclcluster.1tsdljh.mongodb.net/todo?retryWrites=true&w=majority',
+    ),
+    TodoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/todo/dtos/create-todo.dto.ts
@@ -0,0 +1,17 @@
+import { IsNotEmpty, IsString, IsEnum, IsOptional } from 'class-validator';
+
+import { TodoStatus } from '../schemas/todo.schemas';
+
+export class CreateTodoDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsEnum(TodoStatus)
+  @IsOptional()
+  status?: TodoStatus;
+}
